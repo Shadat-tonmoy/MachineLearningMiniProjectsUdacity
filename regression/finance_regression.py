26479278f155b6d81d3885b60db2e1d240000695
Shadat Tonmoy <shadat.tonmoy@gmail.com>
2018-08-19 20:30:50 +0600

    Lesson 8 Regression Algorithm with practice is completed

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -29,7 +29,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,12 +38,13 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
-
-
-
-
-
-
+from sklearn import linear_model 
+reg = linear_model.LinearRegression()
+reg.fit(feature_train,target_train)
+print "Slope ",reg.coef_
+print "Intercept ",reg.intercept_
+print "Score on Train",reg.score(feature_train,target_train)
+print "Score on Test",reg.score(feature_test,target_test)
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
@@ -63,8 +64,12 @@ plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
 try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
+    print "Exception Occured"
     pass
 plt.xlabel(features_list[1])
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b") 
+print "New Slope ",reg.coef_
 plt.ylabel(features_list[0])
 plt.legend()
 plt.show()
